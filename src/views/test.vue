3b6d1892df0412b817fdf44127bfe32c71ae2322
齐文涛-qi <1306925692@qq.com>
2024-04-10 16:47:53 +0800

    fix

--- src/views/test.vue
+++ src/views/test.vue
@@ -15,7 +15,8 @@
           </el-form-item>
         </el-form>
         <span>剩余可用频率：</span>
-        <span>{{ restData.length?restData.join(','):'-' }}</span>
+        <div v-for="item,index in restDataRe" :key="index">{{ item.join(",") }}</div>
+        <!-- <div> {{ restData.length ? restData.join(",") : "-" }}</div> -->
         
       </div>
     </div>
@@ -26,11 +27,11 @@
           type="file"
           style="display: block; margin-bottom: 10px"
         />
-        
+
         <el-button size="mini" @click="handleAdd()">添加点位</el-button>
-        <el-button style="margin-top:10px" size="mini" @click="exportData()"
-              >导出</el-button
-            >
+        <el-button style="margin-top: 10px" size="mini" @click="exportData()"
+          >导出</el-button
+        >
       </div>
       <el-table :data="pointsList" style="width: 100%; margin-top: 60px">
         <el-table-column type="index"> </el-table-column>
@@ -100,9 +101,9 @@ export default {
       doingIndex: 0,
       circleMarkers: [], //点位图像数组
       circlePoints: "", //圆圈对象
-      x:116.433322,
-      y:39.900255,
-      r:50000,
+      x: 116.433322,
+      y: 39.900255,
+      r: 50000,
     };
   },
   created() {},
@@ -113,34 +114,45 @@ export default {
   unmounted() {
     this.map.destroy();
   },
+  computed:{
+    restDataRe(){
+     return  this.restData.reduce((acc, curr, index) => {
+    const groupIndex = Math.floor(index / 7);
+    if (!acc[groupIndex]) {
+        acc[groupIndex] = [];
+    }
+    acc[groupIndex].push(curr);
+    return acc;
+}, []);
+    }
+  },
   watch: {
     allData: {
       handler(newValue, oldValue) {
         this.pasterMap();
-        this.compare()
+        this.compare();
       },
       deep: true,
     },
-    x(){
-      this.drawCircle()
+    x() {
+      this.drawCircle();
     },
-    y(){
-      this.drawCircle()
+    y() {
+      this.drawCircle();
     },
-    r(){
-      this.drawCircle()
-    }
+    r() {
+      this.drawCircle();
+    },
   },
   methods: {
     async initAMap() {
-      let that = this
+      let that = this;
       await AMapLoader.load({
         key: "7e1b85237d72d57b3ec1d649ffa3f648", // 申请好的Web端开发者Key，首次调用 load 时必填
         version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
         plugins: ["AMap.Scale"], //需要使用的的插件列表，如比例尺'AMap.Scale'，支持添加多个如：['...','...']
       })
         .then((AMap) => {
-          
           var roadNet = new AMap.TileLayer.RoadNet();
           var satellite = new AMap.TileLayer.Satellite();
           this.map = new AMap.Map("container", {
@@ -160,10 +172,10 @@ export default {
             }, //OGC 标准的 WMS 地图服务的 GetMap 接口的参数
           });
           this.map.add(wms);
-          this.map.on('click', function(e) {
-            that.x=e.lnglat.getLng()
-            that.y=e.lnglat.getLat()
-            that.drawCircle()
+          this.map.on("click", function (e) {
+            that.x = e.lnglat.getLng();
+            that.y = e.lnglat.getLat();
+            that.drawCircle();
           });
         })
         .catch((e) => {
@@ -171,9 +183,12 @@ export default {
         });
     },
     drawCircle() {
-      this.map.remove(this.circlePoints)
+      this.map.remove(this.circlePoints);
       //设置圆形位置
-      var center = new AMap.LngLat(this.x?this.x:116.397428, this.y?this.y:39.90923);
+      var center = new AMap.LngLat(
+        this.x ? this.x : 116.397428,
+        this.y ? this.y : 39.90923
+      );
       //设置圆的半径大小
       var radius = this.r;
       this.circlePoints = new AMap.Circle({
@@ -191,28 +206,27 @@ export default {
       });
       this.map.add(this.circlePoints);
       this.map.setFitView([this.circlePoints]);
-      this.compare()
+      this.compare();
     },
     // 计算剩余频率
-    compare(){
-      this.restData = this.allData
-      let p1 = [this.x,this.y]
-      this.pointsList.forEach(item=>{
-        let p2 = [item.x,item.y]
-        let meter = AMap.GeometryUtil.distance(p1,p2)
-        if(meter<this.r){
-          this.restData = this.restData.filter(data=>{
-            let vs = item.v.split(',')
-            return !vs.some(v=>v==data)
+    compare() {
+      this.restData = this.allData;
+      let p1 = [this.x, this.y];
+      this.pointsList.forEach((item) => {
+        let p2 = [item.x, item.y];
+        let meter = AMap.GeometryUtil.distance(p1, p2);
+        if (meter < this.r) {
+          this.restData = this.restData.filter((data) => {
+            let vs = item.v.split(",");
+            return !vs.some((v) => v == data);
             // return data!=item.v
-          })
-          console.log(this.restData);
+          });
         }
-      })
+      });
     },
     // 全部频率更新
-    changeAllDataStr(){
-      this.allData = this.allDataStr.split(',').filter(item=>item)
+    changeAllDataStr() {
+      this.allData = this.allDataStr.split(",").filter((item) => item);
     },
     handleFileChange() {
       let that = this;
@@ -253,26 +267,35 @@ export default {
       this.pointsList.push({ x: "116.3912757", y: "39.906217", v: "" });
       console.log(this.pointsList);
       this.pasterMap();
-      this.drawCircle()
-      this.compare()
+      this.drawCircle();
+      this.compare();
     },
     handleEdit(row, index) {
       this.dialogVisible = true;
       this.doingIndex = index;
-      this.drawCircle()
-      this.compare()
+      this.drawCircle();
+      this.compare();
     },
     handleDel(val, index) {
       this.pointsList.splice(index, 1);
       this.pasterMap();
-      this.drawCircle()
-      this.compare()
+      this.drawCircle();
+      this.compare();
     },
     pasterMap() {
       this.circleMarkers.forEach((circleMarker) => {
         this.map.remove(circleMarker);
       });
 
+      this.pointsList.forEach(item=>{
+        console.log(item.v,this.allData);
+        item.v.split(',').forEach(v=>{
+          if(this.allData.indexOf(v)==-1) {
+          this.allData.push(v)
+        }
+        })
+        this.allDataStr = this.allData.join(",");
+      })
       this.circleMarker = [];
       var radius = 5; //圈半径
       let centers = this.pointsList.map((item) => {
@@ -298,24 +321,24 @@ export default {
         this.map.add(circleMarker);
         // this.map.setFitView([circleMarker]);
       });
-      this.compare()
+      this.compare();
     },
     // 导出
-    exportData(){
+    exportData() {
       console.log(this.pointsList);
-      var data = ''
-      this.pointsList.forEach((item,index)=>{
-        if(index+1<this.pointsList.length){
-          data+=`${item.x} ${item.y}:${item.v.replaceAll(',',' ')},\n`
-        }else{
-          data+=`${item.x} ${item.y}:${item.v.replaceAll(',',' ')}\n`
+      var data = "";
+      this.pointsList.forEach((item, index) => {
+        if (index + 1 < this.pointsList.length) {
+          data += `${item.x} ${item.y}:${item.v.replaceAll(",", " ")},\n`;
+        } else {
+          data += `${item.x} ${item.y}:${item.v.replaceAll(",", " ")}\n`;
         }
-      })
-      data+='/\n'
-      data+=this.allData.join(' ')
-      data+='\n/剩余频率\n'
-      data+=this.restData.join(' ')
-      let str = new Blob([data], {type: 'text/plain;charset=utf-8'});
+      });
+      data += "/\n";
+      data += this.allData.join(" ");
+      data += "\n/剩余频率\n";
+      data += this.restData.join(" ");
+      let str = new Blob([data], { type: "text/plain;charset=utf-8" });
       // 注意这里要手动写上文件的后缀名
       saveAs(str, `点位统计.txt`);
     },
